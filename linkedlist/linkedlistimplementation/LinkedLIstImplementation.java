322840f72a9632800417b08214345dcc9ad3759d
shindesurajjotiram <suraj.jotiram@godigit.com>
2024-07-29 11:30:10 +0530

    the linked list map implementation is done

---
+++ linkedlist/linkedlistimplementation/LinkedLIstImplementation.java
@@ -0,0 +1,18 @@
+package linkedlist.linkedlistimplementation;
+
+public class LinkedLIstImplementation {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert( 1);
+        list.insert( 2);
+        list.insert( 2);
+        list.insert( 5);
+        list.insert( 5);
+        list.insert( 6);
+        list.insert( 7);
+        list.insert( 8);
+
+        System.out.println(list);
+    }
+}
