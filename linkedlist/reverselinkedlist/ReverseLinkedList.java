28d07f1ca445529b57b4c241949cfc7d91cd17d2
shindesurajjotiram <suraj.jotiram@godigit.com>
2024-07-29 12:00:25 +0530

    ReversedLinkedList class is added

---
+++ linkedlist/reverselinkedlist/ReverseLinkedList.java
@@ -0,0 +1,20 @@
+package linkedlist.reverselinkedlist;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert( 1);
+        list.insert( 2);
+        list.insert( 2);
+        list.insert( 5);
+        list.insert( 5);
+        list.insert( 6);
+        list.insert( 7);
+        list.insert( 8);
+
+        list.reverse();
+
+        System.out.println(list);
+    }
+}
